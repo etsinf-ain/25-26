337df9575611b0399b58861d56bd966200437721
mrebollo <mrebollo@upv.es>
2026-03-14 12:26:55 +0100

    feat: solutions to activities and samples of P2

--- lab/p2_spadebdi/hellow.py
+++ lab/p2_spadebdi/hellow.py
@@ -3,7 +3,7 @@ from spade import agent
 from spade_bdi.bdi import BDIAgent
 
 async def main():
-    agent = BDIAgent("hello@localhost", "1234", "hello_world.asl",port=5200)
+    agent = BDIAgent("hello@localhost", "1234", "hello_world.asl")
     await agent.start()
 
 if __name__ == "__main__":
